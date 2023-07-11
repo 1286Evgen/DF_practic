11d41778ce74445b02304febdd40d65d2d993df7
1286Evgen <lopatingenia@rambler.ru>
2023-07-11 15:33:53 -0300

    headerstart

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,7 +4,7 @@ import {Link} from "../Nav";
 import "./index.css";
 
 const Logo = ({
-    position = "vertical" // horizontal vertical
+    position = "horizontal" // horizontal vertical
 
 }) => {
     return (
