8237063892c5bfe0300f8ff28f6cb682f9686efc
1286Evgen <lopatingenia@rambler.ru>
2023-07-14 11:59:06 -0300

    head8

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,31 @@
+import {useContext} from "react";
 import Layout from "../Layout";
 import Logo from "../Logo";
 import Nav from "../Nav";
 import {Search} from "../Form";
+
+import MainCtx from "../../context/main";
+
 import menu from "../../assets/data/menu.json";
 
 import "./index.css";
 
 const Header = () => {
+    const {screen} = useContext(MainCtx);
     return (
         <header className="header">
             <Layout>
                 <Logo/>
                 <Search/>
-                <button className="card__btn">
-                    <i className="lni lni-menu"/>
-                </button>
-                <Nav menu={menu.header}/>
+                {screen < 1064
+                    ? <>
+                        <button className="card__btn">
+                            <i className="lni lni-menu"/>
+                        </button>
+                        <Nav menu={menu.header}/>
+                    </>
+                    : <Nav menu={menu.header}/>
+                }
             </Layout>
         </header>
     )
